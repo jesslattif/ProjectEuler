78b695b786fb20d490e0d7416dec6767ab4b11c1
Jessica <jesslattif@gmail.com>
2013-05-17 14:41:48 -0700

    correct solution to prob 2

--- Problem2.py
+++ Problem2.py
@@ -1,26 +1,35 @@
 """Find the sum of all even fibonacci numbers below 4 million"""
 
-# total = 0
-# for num in range(1, 1000):
-# 	if num % 3 == 0 or num %5 == 0:
-# 		total = total + num 
-# print total
-
-
 #fibonacci:
 
-def fibonacci(n):
-    if n <= 2:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+import math
 
-# mylist = []
-for i in xrange(1, 20):
-	mylist = [fibonacci(i) if fibonacci(i % 2) == 0]:
-#     	mylist.append(i)
-#     else:
-#     	pass
+mylist = []
 
-	print mylist
-	# print fibonacci(i)
+def fibo():
+# """This is the long & slow way"""
+# # if n == 0:
+# #     return 0
+# # elif n == 1:
+# # 	return 1
+# # else:
+# #     return fibo(n - 1) + fibo(n - 2)
+# """ this is the faster way """
+	current = 0
+	last = 1
+	while current <= 4000000:
+		temp = current
+		current = current + last
+		last = temp
+		mylist.append(current)
+		current += 0
+
+sumlist = []
+def sum_even_fibos():
+	for i in mylist:
+		if i % 2 == 0:
+			sumlist.append(i)
+	print sum(sumlist)
+
+
+
